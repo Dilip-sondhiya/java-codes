c6fc53ad2fb85a66a3f3be570e53f0169e8fccbd
Dilip-sondhiya <dilipsondhiya2906@gmail.com>
2026-06-09 15:11:43 +0530

    Add files via upload

---
+++ dilip.java
@@ -0,0 +1,12 @@
+class Dilip {
+public static void main(String args[]){
+System.out.println(" hello indore ");
+int a = 10,b = 20;
+int c = a + b;
+System.out.println(" sum is "+c);
+}
+}
+
+class A{
+    public static  void main(strin args
+}
